b38b402c8e2426744e6a70d5f0f3e2567d8a976d
Bikram Lama <thebikramlama1123@gmail.com>
2020-05-13 02:40:55 +0545

    Removed Delays

--- cors.php
+++ cors.php
@@ -5,4 +5,4 @@ header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Max-Age: 86400');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 header('Content-Type: application/json');
-sleep(rand(0,3)); // Simulate A delay 0-3 seconds
+//sleep(rand(0,3)); // Simulate A delay 0-3 seconds
